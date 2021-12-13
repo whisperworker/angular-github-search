474a5c2a298af9ecf7598e1c9ab038b9887ac603
whisperworker <thewhisperworker@gmail.com>
2021-12-13 10:44:54 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchRepoComponent } from './components/search-repo/search-repo.component';
+import { RepositoriesComponent } from './components/repositories/repositories.component';
+import { RepoItemComponent } from './components/repo-item/repo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchRepoComponent,
+    RepositoriesComponent,
+    RepoItemComponent
   ],
   imports: [
     BrowserModule,
