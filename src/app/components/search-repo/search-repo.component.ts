4aa6f6c2c15b69bf0b5260da4c1a32a9c3ac585c
whisperworker <thewhisperworker@gmail.com>
2021-12-13 10:42:50 +0300

    Add search component

---
+++ src/app/components/search-repo/search-repo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-repo',
+  templateUrl: './search-repo.component.html',
+  styleUrls: ['./search-repo.component.css']
+})
+export class SearchRepoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
