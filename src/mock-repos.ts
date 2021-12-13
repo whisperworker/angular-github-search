0fd404c1fd1efc3a93a4af75499927729227d4ef
whisperworker <thewhisperworker@gmail.com>
2021-12-13 10:46:30 +0300

    add mock data

---
+++ src/mock-repos.ts
@@ -0,0 +1,52 @@
+import {Repository} from "./Repository";
+
+
+export const REPOS: Repository[] = [
+  {
+    id: 6154722,
+    name: "microsoft/TypeScript",
+    createdDate: "2014-06-17T15:28:39Z",
+    updatedDate: "2021-12-13T06:56:57Z",
+    pushedDate: "2021-12-13T07:03:10Z",
+    description: "TypeScript is a superset of JavaScript that compiles to clean JavaScript output.",
+    language: "TypeScript",
+    owner: {
+      id: 6154722,
+      login: "microsoft"
+    },
+    size: 1601158,
+    stars: 76554
+  },
+
+  {
+    id: 460078,
+    name: "angular/angular",
+    createdDate: "2010-01-06T00:34:37Z",
+    updatedDate: "2021-12-12T23:26:54Z",
+    pushedDate: "2021-12-09T23:20:01Z",
+    description: "AngularJS - HTML enhanced for web apps!",
+    language: "JavaScript",
+    owner: {
+      id: 139426,
+      login: "angular"
+    },
+    size: 101541,
+    stars: 59579
+  },
+
+  {
+    id: 63476337,
+    name: "TheAlgorithms/Python",
+    createdDate: "2016-07-16T09:44:01Z",
+    updatedDate: "2021-12-13T07:22:32Z",
+    pushedDate: "2021-12-13T06:23:41Z",
+    description: "All Algorithms implemented in Python",
+    language: "Python",
+    owner: {
+      id: 20487725,
+      login: "TheAlgorithms"
+    },
+    size: 12487,
+    stars: 125390
+  }
+]
