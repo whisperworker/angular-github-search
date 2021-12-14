a883ec4f2fce7181728a039612a5af6eb09fec82
whisperworker <thewhisperworker@gmail.com>
2021-12-14 14:56:54 +0300

    code cleanup, add validation of searchQuery

--- src/app/services/repository.service.ts
+++ src/app/services/repository.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Repository} from "../../Repository";
 
 const httpOptions = {
   headers:new HttpHeaders({
@@ -18,6 +17,9 @@ export class RepositoryService {
   constructor(private http: HttpClient) { }
 
   getReposByName(searchQuery: string = "stars:>1"):Observable<any> {
+    if(!searchQuery) {
+      searchQuery = "stars:>1";
+    }
     return this.http.get<any>(this.apiUrl, {
       params: new HttpParams()
         .set(`q`, searchQuery)
