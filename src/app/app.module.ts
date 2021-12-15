6e6a66b59f8e535978e9826df4dee75b70e35442
whisperworker <thewhisperworker@gmail.com>
2021-12-15 19:54:58 +0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {AppRoutingModule} from './app-routing.module';
+import {RouterModule, Routes} from "@angular/router";
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
@@ -10,6 +11,15 @@ import {SearchRepoComponent} from './components/search-repo/search-repo.componen
 import {RepositoriesComponent} from './components/repositories/repositories.component';
 import {RepoItemComponent} from './components/repo-item/repo-item.component';
 import {LoaderComponent} from './components/loader/loader.component';
+import {RepoFiltersComponent} from './components/repo-filters/repo-filters.component';
+import {RepoPageComponent} from './components/repo-page/repo-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path: '', component: RepositoriesComponent},
+  {path: `info`, component: RepoPageComponent},
+  {path: '**', component: PageNotFoundComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,13 +28,20 @@ import {LoaderComponent} from './components/loader/loader.component';
     SearchRepoComponent,
     RepositoriesComponent,
     RepoItemComponent,
-    LoaderComponent
+    LoaderComponent,
+    RepoFiltersComponent,
+    RepoPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
